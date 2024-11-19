a0cd0033b20067080336e07a220443918a6cd5cb
sebastianpgab <sebastianpgab@gmail.com>
2024-11-19 23:30:31 +0100

    Adjust mobile menu: dropdown fits number of subcategories

--- src/app/core/navbar/navbar.component.ts
+++ src/app/core/navbar/navbar.component.ts
@@ -1,19 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
+
+interface Category {
+  title: string;
+  path: string;
+}
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./style.scss'
-  ]
+  styleUrls: ['./style.scss']
 })
 export class NavbarComponent {
 
-  
+  menuOpen: boolean = false;
+  dropdownOpen: boolean = false;
+
+  categories: Category[] = [
+    { title: 'Lufa', path: '/r8/barrel' },
+    { title: 'Kolba', path: '/r8/stock' },
+    { title: 'Optyka', path: '/r8/optics' },
+    { title: 'Akcesoria', path: '/r8/accessories' },
+    { title: 'Podsumowanie', path: '/r8/summary' }
+  ];
+
+  @ViewChild('dropdownButton') dropdownButton!: ElementRef;
+
   constructor(public router: Router) {}
 
   isHomePage(): boolean {
     return this.router.url === '/' || this.router.url === '/home';
   }
 
+  toggleMenu(): void {
+    this.menuOpen = !this.menuOpen;
+    if (this.menuOpen) {
+      this.dropdownOpen = true; 
+    } else {
+      this.dropdownOpen = false;
+    }
+  }
+
+  toggleDropdown(): void {
+    this.dropdownOpen = !this.dropdownOpen;
+  }
+
+  navigateTo(category: Category): void {
+    this.router.navigate([category.path]);
+    this.menuOpen = false;
+    this.dropdownOpen = false;
+  }
 }
