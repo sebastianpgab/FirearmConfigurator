2dc794ee43e2cb78dcbde22a32a52a305501a9c1
sebastianpgab <sebastianpgab@gmail.com>
2024-11-25 23:30:33 +0100

    Add accessory options handling

--- src/app/R8/accessory/accessory.component.ts
+++ src/app/R8/accessory/accessory.component.ts
@@ -1,10 +1,79 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Option } from "../../option/model";
+import { AccessoryService } from "./accessory.service";
+import { Router } from "@angular/router";
+import { Rifle } from "../rifle/model";
 
 @Component({
-  selector: 'app-accessory',
-  templateUrl: './accessory.component.html',
-  styles: [
-  ]
+  selector: "app-accessory",
+  templateUrl: "./accessory.component.html",
+  styleUrls: [],
 })
-export class AccessoryComponent {
+export class AccessoryComponent implements OnInit {
+
+  rifles: Rifle[] = [];
+  features: any;
+  gunCases: Option[] = [];
+  softGunCovers: Option[] = [];
+  rifleSlings: Option[] = [];
+
+  selectedRifle: Rifle | null = null;
+  selectedGunCase: Option | null = null;
+  selectedSoftGunCover: Option | null = null;
+  selectedRifleSling: Option | null = null;
+
+  isDisabledSoftGunCover: boolean = true;
+  isDisabledRifleSling: boolean = true;
+
+  constructor(
+    private accessoryService: AccessoryService,
+    private router: Router
+  ) {}
+
+  ngOnInit() {
+    const savedRifle = JSON.parse(sessionStorage.getItem('selectedRifle') || 'null');
+
+    this.accessoryService.getData().subscribe(
+      (data) => {
+        this.features = data.features;
+        this.rifles = data.rifles;
+
+        // Ustawienie wybranej strzelby
+        this.selectedRifle = savedRifle ? savedRifle : this.rifles[0];
+
+        // Load options
+        this.gunCases = this.features["gunCases"];
+        this.softGunCovers = this.features["softGunCovers"];
+        this.rifleSlings = this.features["rifleSlings"];
+
+        // Update option states
+        this.updateOptionStates();
+      },
+      (error) => {
+        console.error("Error loading accessory data:", error);
+      }
+    );
+  }
+
+  onNext(): void {
+    // Save selected options to sessionStorage
+    const selectedOptions = {
+      gunCase: this.selectedGunCase,
+      softGunCover: this.selectedSoftGunCover,
+      rifleSling: this.selectedRifleSling,
+    };
+    sessionStorage.setItem("selectedAccessories", JSON.stringify(selectedOptions));
+
+    // Navigate to the next step
+    this.router.navigate(["/next-step"]);
+  }
+
+  onBack() {
+    this.router.navigate(["/r8/chamberBolt"]);
+    }
+
+  public updateOptionStates(): void {
+    this.isDisabledSoftGunCover = !this.selectedGunCase;
+    this.isDisabledRifleSling = !this.selectedSoftGunCover;
+  }
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import { HomeComponent } from './home/home.component';
 import { BarrelComponent } from './R8/barrel/barrel.component';
 import { StockComponent } from './R8/stock/stock.component';
 import { ChamberBoltComponent } from './R8/chamber-bolt/chamber-bolt.component';
+import { AccessoryComponent } from './R8/accessory/accessory.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   {path: 'r8/barrel', component: BarrelComponent},
   {path: 'r8/stock', component: StockComponent},
   {path: 'r8/chamberBolt', component: ChamberBoltComponent},
+  {path: 'r8/accessory', component: AccessoryComponent},
   { path: 'home', component: HomeComponent },  
   { path: '**', redirectTo: '/home' }
 ]
