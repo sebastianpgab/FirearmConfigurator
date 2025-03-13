1ebb1fee112184d8b67343bc0a06aff9e446d640
sebastianpgab <sebastianpgab@gmail.com>
2025-03-13 23:31:23 +0100

    Add configuration summary section

--- src/app/summary/summary.component.ts
+++ src/app/summary/summary.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ConfiguratorService } from '../core/services/configurator.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-summary',
@@ -8,4 +10,14 @@ import { Component } from '@angular/core';
 })
 export class SummaryComponent {
 
+  private subscription!: Subscription; 
+  state: any;
+  constructor(private configuratorService: ConfiguratorService) {}
+
+  ngOnInit(): void {
+    // Subskrybujemy stan
+    this.subscription = this.configuratorService.state$.subscribe((state) => {
+      this.state = state; 
+    });
+  }
 }
